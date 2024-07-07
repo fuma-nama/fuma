fea838ac1ece408cb53fe19db3bb63cdd4c37280
fuma-nama <god63820869@gmail.com>
2024-07-07 10:11:29 +0800

    Blog: How to write a good docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { withContent } from "@fuma-content/next";
 import rehypeShiki from "@shikijs/rehype";
+import remarkGfm from "remark-gfm";
 
 export default withContent({
   content: {
     files: ["./content/**/*"],
     outputDir: "./.content",
     mdxOptions: {
+      remarkPlugins: [remarkGfm],
+      // @ts-expect-error
       rehypePlugins: [[rehypeShiki, { theme: "github-dark" }]],
     },
   },

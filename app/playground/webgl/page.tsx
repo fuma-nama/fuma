724c727af761c9997af6004d8c5d2b33f818f925
fuma-nama <god63820869@gmail.com>
2024-08-02 20:12:46 +0800

    Add credit

--- app/playground/webgl/page.tsx
+++ app/playground/webgl/page.tsx
@@ -6,6 +6,14 @@ export default function Page() {
     <>
       <Shell>
         <Canvas className="size-full" />
+        <a
+          href="https://x.com/stormoid"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="absolute right-4 bottom-4 text-xs text-neutral-100/50"
+        >
+          @stormoid
+        </a>
       </Shell>
       <ExitBar />
     </>

53869a9f9668c45f165330ceb5114ba3230b36b6
Fuma Nama <fuma-nama@noreply.com>
2026-02-01 22:20:34 +0800

    fix legacy packages

--- components/spotify.tsx
+++ components/spotify.tsx
@@ -1,2 +1,62 @@
+/**
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 "use client";
-export { Spotify } from "react-spotify-embed";
+import { type HTMLAttributes } from "react";
+
+interface SpotifyProps extends HTMLAttributes<HTMLIFrameElement> {
+  [key: string]: any
+
+  link: string
+  wide?: boolean
+  width?: number | string
+  height?: number | string
+  frameBorder?: number | string
+  allow?: string
+}
+
+export const Spotify = ({
+  link,
+  style = {},
+  wide = false,
+  width = wide ? "100%" : 300,
+  height = wide ? 80 : 380,
+  frameBorder = 0,
+  allow = "encrypted-media",
+  ...props
+}: SpotifyProps) => {
+  const url = new URL(link);
+  // https://open.spotify.com/track/1KFxcj3MZrpBGiGA8ZWriv?si=f024c3aa52294aa1
+  // Remove any additional path segments
+  url.pathname = url.pathname.replace(/\/intl-\w+\//, "/");
+  return (
+    <iframe
+      title="Spotify Web Player"
+      src={`https://open.spotify.com/embed${url.pathname}`}
+      width={width}
+      height={height}
+      frameBorder={frameBorder}
+      allow={allow}
+      style={{
+        borderRadius: 8,
+        ...style
+      }}
+      {...props}
+    />
+  );
+};

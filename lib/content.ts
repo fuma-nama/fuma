c323643286719c97c130cb88f2661e0a701242af
Fuma Nama <fuma-nama@noreply.com>
2026-02-19 22:48:35 +0800

    Bump deps

--- lib/content.ts
+++ lib/content.ts
@@ -1,7 +1,7 @@
 import { blog } from "content/blog";
 import path from "node:path";
 
-export const blogPosts = blog.transform((item) => ({ ...item, slug: idToSlug(item.id) }));
+export const blogPosts = blog.transform((id, item) => [id, { ...item, slug: idToSlug(item.id) }]);
 
 export function idToSlug(id: string) {
   return path.basename(id, path.extname(id));
